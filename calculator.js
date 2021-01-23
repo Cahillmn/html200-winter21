d4ab3e3c614f1476e0917f58ade811d73029f586
Cahillmn <mcahill@seattleu.edu>
2021-01-23 09:22:13 -0800

    added Javascript

---
+++ calculator.js
@@ -0,0 +1,19 @@
+let x = 2;
+let y = 4;
+let a = 5;
+let b = 1;
+
+//Add
+console.log(x + y);
+
+//Subtract
+console.log(a - b)
+
+//Multiply
+console.log(y * a);
+
+//Divide
+console.log(y / x);
+
+//Combination
+console.log((a + b) / x);
