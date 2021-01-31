db13a805dcbeeb6cf1c8c8f2f2fd71197b5243b1
Cahillmn <mcahill@seattleu.edu>
2021-01-31 14:12:35 -0800

    finished lesson03

---
+++ lesson03/excercise-two.js
@@ -0,0 +1,7 @@
+let arr = [[ 'Corgis', 'are', 'the', 'best', 'dogs'], ['.']];
+console.log(arr)
+
+let str = arr[0].join(' ') + arr[1].join( );
+console.log(str);
+
+console.log(arr[0][2]);
---
+++ lesson03/exercise-one.js
@@ -0,0 +1,10 @@
+let arr = ['dog', 'cat' , 'bird', 'fish', 'hamster'];
+
+arr.push('bunny');
+console.log(arr);
+
+arr.splice(2, 1);
+console.log(arr);
+
+let str= arr.join(' , ');
+console.log(str);
