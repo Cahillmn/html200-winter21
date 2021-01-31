3ac2253357fb21c0598f726dc50000c6d7f4990f
Cahillmn <mcahill@seattleu.edu>
2021-01-31 15:45:12 -0800

    finished assignment03

--- lesson03/excersise-three.js
+++ lesson03/excersise-three.js
@@ -1,23 +1,7 @@
-let animals = ['dog', 'cat' , 'bird', 'fish', 'hamster'];
+let animals = ['dog', 'cat', 'bird', 'fish', 'hamster'];
 
 for (let i = 0; i < animals.length; i++) {
-  
-  for (let j = 0; j < animals.length; j++ ) {
-  
-  let position = '' ;
-   if (j === 0) {
-            position = 1;
-        } else if (j === 1) {
-            position = 2;
-        } else if (j === 2) {
-            position = 3;
-        } else if (j === 3) {
-            position = 4;
-        } else if (j === 4){
-            position = 5;
-        }
-  
-  console.log (position + ', ' + animals[i]);
-}
-  
+
+    console.log(i+1 + ',' + animals[i]);
+
 }
