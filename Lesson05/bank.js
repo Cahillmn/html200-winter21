f4a4d0c43a935cc625622c80b4b14f8afe9199f2
Cahillmn <mcahill@seattleu.edu>
2021-02-14 17:16:14 -0800

    finished bank app

--- Lesson05/bank.js
+++ Lesson05/bank.js
@@ -1,32 +1,32 @@
-function bank() {
+let bankOpen = true;
+let balance = 1000;
+
+while (bankOpen) {
     const option = prompt('Enter Q to quit, W to withdraw, D to deposit, or B to view balance ');
-    let balance = 25000;
     switch (option) {
         case 'Q':
             alert('You quit');
+            bankOpen = false;
             break;
-        case 'W': {
-            const amount = prompt('Enter an amount to withdraw');
-            alert('You are withdrawing: $' + amount);
-            const option = prompt('Enter Q to quit, W to withdraw, D to deposit, or B to view balance ');
-        }
-        break;
-        case 'D': {
-            const amount = prompt('Enter an amount to deposit');
-            alert('You are depositing: $' + amount);
-            const option = prompt('Enter Q to quit, W to withdraw, D to deposit, or B to view balance ');
-        }
-        break;
-        case 'B': {
+        case 'B':
             alert('Your balance is: $' + balance);
-            const option = prompt('Enter Q to quit, W to withdraw, D to deposit, or B to view balance ');
-        }
-        break;
+            break;
+        case 'W':
+            const amountWithdrawn = prompt('How much do you want to withdraw?');
+            if (balance - amountWithdrawn < 0 ) {
+              alert('You have exceeded your available balance');  
+            } else {(balance = balance - amountWithdrawn);
+            alert('You are withdrawing: $' + amountWithdrawn + ' your remaining balance is: $' + balance);
+            }
+            break;
+        case 'D':
+            const depositAmount = prompt('How much do you want to deposit?');
+            balance = balance + Number(depositAmount);
+            alert('You are depositing: $' + depositAmount + ' your total balance is: $' + balance)
+            break;
         default:
             alert('Sorry');
-            const option = prompt('Enter Q to quit, W to withdraw, D to deposit, or B to view balance ');
-        break;
-
+            break;
+        }     
     }
-
-}
+    
