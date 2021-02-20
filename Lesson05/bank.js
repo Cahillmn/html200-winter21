9c5bb05e5804815d11a866c033c3d8a3d1bffd72
Cahillmn <mcahill@seattleu.edu>
2021-02-20 10:43:58 -0800

    added Number to depositAmount

--- Lesson05/bank.js
+++ Lesson05/bank.js
@@ -13,9 +13,9 @@ while (bankOpen) {
             break;
         case 'W':
             const amountWithdrawn = prompt('How much do you want to withdraw?');
-            if (balance - amountWithdrawn < 0 ) {
+            if (balance - Number(amountWithdrawn) < 0 ) {
               alert('You have exceeded your available balance');  
-            } else {(balance = balance - amountWithdrawn);
+            } else {(balance = balance - Number(amountWithdrawn));
             alert('You are withdrawing: $' + amountWithdrawn + ' your remaining balance is: $' + balance);
             }
             break;
